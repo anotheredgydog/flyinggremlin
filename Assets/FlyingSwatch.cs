f68318a8a30fb6212df131f81ddd0f32872dadb9
Abby <aalperovich@dignitastechnologies.com>
2020-08-11 23:08:38 -0400

    First commit

---
+++ Assets/FlyingSwatch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FlyingSwatch : MonoBehaviour {
+    public float speed;
+    public int score;
+    public UnityEvent onScoreUpdated;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Time.deltaTime * new Vector3(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"),
+            0.0f
+        ) * speed;
+
+        if (Input.GetKey(KeyCode.Y))
+        {
+            AddScore(1);
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            AddScore(1);
+        }
+    }
+
+    public void AddScore(int s)
+    {
+        score += s;
+        onScoreUpdated.Invoke();
+    }
+}
---
+++ Assets/ScoreDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour {
+    public FlyingSwatch gremlin;
+    public UnityEngine.UI.Text text;
+
+    private void Awake()
+    {
+        UpdateScore();
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        gremlin.onScoreUpdated.AddListener(UpdateScore);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateScore()
+    {
+        text.text = "Score: " + gremlin.score;
+    }
+}
